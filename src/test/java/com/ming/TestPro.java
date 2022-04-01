a2a889344a44913894e53718658ba1f6845cd57b
liming <522724933@qq.com>
2022-04-01 17:15:03 +0800

    #2022-04-01

--- src/test/java/com/ming/TestPro.java
+++ src/test/java/com/ming/TestPro.java
@@ -1,14 +1,17 @@
 package com.ming;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.ming.entities.*;
 import com.ming.util.PercentUtil;
+import org.assertj.core.util.Lists;
+import org.json.JSONObject;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import java.util.stream.Collectors;
 
 public class TestPro {
 
@@ -45,12 +48,12 @@ public class TestPro {
     }
 
 
-    public static Double billingFormula(double number , EnergyType decimalPlace ) {
+    public static Double billingFormula(double number, EnergyType decimalPlace) {
 
 
         Double fm = 0.00;
         switch (decimalPlace) {
-            case  water:
+            case water:
                 //  水
                 fm = number * 5.8;
                 break;
@@ -65,7 +68,123 @@ public class TestPro {
                 break;
         }
 
-        return  fm;
+        return fm;
 
     }
+
+
+    @Test
+    public void DishTest() {
+        List<Dish> list = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            Dish dish = new Dish();
+            if (i % 2 == 0) {
+                dish.setName("照明用电" + i);
+                dish.setVegetarian(true);
+                dish.setCalories(i);
+                dish.setType(new Type(Integer.toString(i), "电"));
+            } else {
+
+                dish.setName("水井" + i);
+                dish.setVegetarian(true);
+                dish.setCalories(i);
+                dish.setType(new Type(Integer.toString(i), "水"));
+            }
+            list.add(dish);
+        }
+
+        //System.out.println(JSONArray.parseArray(JSON.toJSONString(list)));
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+        //1.电量大于2
+        for (Dish dish : list) {
+            if (dish.getCalories() > 2) {
+                lowCaloricDishes.add(dish);
+            }
+        }
+        System.out.println("过滤：" + lowCaloricDishes);
+        //2.对于电量大于2的排序
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+            @Override
+            public int compare(Dish o1, Dish o2) {
+                return Integer.compare(o2.getCalories(), o1.getCalories());
+            }
+        });
+        System.out.println("排序：" + lowCaloricDishes);
+
+        //3.获取排序后的名字
+        List<String> stringList = new ArrayList<>();
+        for (Dish lowCaloricDish : lowCaloricDishes) {
+            stringList.add(lowCaloricDish.getName());
+        }
+        //4.按照种类分类
+        System.out.println("排序后的名称：" + stringList);
+        Map<Type, List<Dish>> map = new HashMap<>();
+        int count = 0;
+        for (Dish lowCaloricDish : lowCaloricDishes) {
+            if (map.get(lowCaloricDish.getType()) == null) {
+                List<Dish> dishList = new ArrayList<>();
+                int calories = lowCaloricDish.getCalories();
+                count += calories;
+                lowCaloricDish.setSum(count);
+                dishList.add(lowCaloricDish);
+                map.put(lowCaloricDish.getType(), dishList);
+            } else {
+                map.get(lowCaloricDish.getType()).add(lowCaloricDish);
+            }
+        }
+        System.out.println("分类：" + map);
+
+        //5.按类型分类计算小计和合计
+        List<Dish> resultList = Lists.newArrayList();
+        Map<String, List<Dish>> collect = list.stream().collect(Collectors.groupingBy(x -> x.getType().getName(), TreeMap::new, Collectors.toList()));
+        collect.forEach((k, v) -> {
+            Map<String, List<Dish>> listMap = v.stream().collect(Collectors.groupingBy(x -> x.getType().getName()));
+            listMap.forEach((x, y) -> {
+                int calories = 0;
+                if (k.equals(x)) {
+                    calories = y.stream().map(Dish::getCalories).reduce(0, (a, b) -> a + b);
+                    Dish dish = new Dish();
+                    dish.setName("小计");
+                    dish.setCalories(calories);
+                    collect.get(x).add(dish);
+                }
+            });
+        });
+        //再总计
+        Set<String> keySet = collect.keySet();
+        keySet.forEach(t -> resultList.addAll(collect.get(t)));
+        //过滤小计的不要求和
+        resultList.stream().filter(t -> !"小计".equals(t.getName())).collect(Collectors.toList());
+        Dish dish = new Dish();
+        dish.setName("合计");
+        dish.setCalories(resultList.stream().filter(t -> !"小计".equals(t.getName())).mapToInt(Dish::getCalories).sum());
+        resultList.add(dish);
+        System.out.println("按类型分类求和：" + resultList);
+    }
+
+    @Test
+    public void dishTest1() {
+        List<Dish> list = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            Dish dish = new Dish();
+            if (i % 2 == 0) {
+                dish.setName("照明用电" + i);
+                dish.setVegetarian(true);
+                dish.setCalories(i);
+                dish.setType(new Type(Integer.toString(i), "电"));
+            } else {
+
+                dish.setName("水井" + i);
+                dish.setVegetarian(true);
+                dish.setCalories(i);
+                dish.setType(new Type(Integer.toString(i), "水"));
+            }
+            list.add(dish);
+        }
+
+        List<String> collect = list.stream().filter(t -> t.getCalories() > 2).sorted(Comparator.comparing(Dish::getCalories)).map(Dish::getName).collect(Collectors.toList());
+
+    }
+
+
 }
