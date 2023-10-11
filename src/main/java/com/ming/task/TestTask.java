07cc983b8dc2227b08eac811c5dd9711b9854858
liming <522724933@qq.com>
2023-10-11 11:43:05 +0800

    #m 数据库计划时间5分钟前抽发

--- src/main/java/com/ming/task/TestTask.java
+++ src/main/java/com/ming/task/TestTask.java
@@ -1,6 +1,8 @@
 package com.ming.task;
 
+import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson.JSON;
 import com.ming.entities.VO.InFoVO;
 import com.ming.mapper.InfoMapper;
@@ -9,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -17,10 +20,20 @@ import java.util.List;
 public class TestTask {
     private final InfoMapper infoMapper;
 
-    @Scheduled(cron = "0 0/2 * * * ?")
+    @Scheduled(cron = "0 0/1 * * * ?")
     public void test1() {
         System.out.println("scheduler1 执行: " + Thread.currentThread() + "-" + DateTime.now());
         List<InFoVO> info = infoMapper.getInfo();
         log.info("数据：{}", JSON.toJSONString(info));
+        info.forEach(x->{
+            //数据库计划时间5分钟前抽发
+            System.out.println(DateUtil.format(x.getTime(), DatePattern.NORM_DATETIME_PATTERN));
+            DateTime dateTime = DateUtil.offsetMinute(x.getTime(), -5);
+            String time = DateUtil.format(dateTime, DatePattern.NORM_DATETIME_MINUTE_PATTERN);
+            String nowTime = DateUtil.format(new Date(), DatePattern.NORM_DATETIME_MINUTE_PATTERN);
+            if(time.equals(nowTime)){
+                log.info("计划时间：{},计划5分钟前时间：{}",x.getTime(),time);
+            }
+        });
     }
 }
