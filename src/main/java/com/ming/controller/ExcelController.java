10ecace1af2d4c1155ea7c264eea97ca06a67b96
liming <522724933@qq.com>
2024-02-28 14:23:05 +0800

    #循环依赖

--- src/main/java/com/ming/controller/ExcelController.java
+++ src/main/java/com/ming/controller/ExcelController.java
@@ -15,6 +15,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +33,7 @@ import java.io.OutputStream;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -41,7 +45,6 @@ public class ExcelController {
 
     @Autowired
     private ExcelService excelService;
-
 
 
     @Autowired
@@ -74,10 +77,11 @@ public class ExcelController {
     }
 
     @GetMapping("/exportList")
-    public void exportList(HttpServletResponse response,String startTime,String endTime) throws IOException {
+    public void exportList(HttpServletResponse response, String startTime, String endTime) throws IOException {
         List<Test> list = iTestService.getList();
+        Map<String, List<Test>> collect = list.stream().collect(Collectors.groupingBy(Test::getName));
         String title = "测试导出.xls";
-        if(StringUtils.isNotBlank(startTime)){
+        if (StringUtils.isNotBlank(startTime)) {
             title = startTime + "-" + endTime + title;
         }
         ExportParams exportParams = new ExportParams();
@@ -86,16 +90,47 @@ public class ExcelController {
         exportParams.setHeight((short) 15);
 
         Workbook result = ExcelExportUtil.exportExcel(exportParams, Test.class, list);
+
+        //再数据末尾填需要额外提示的提示内容
+        Sheet sheet = result.getSheetAt(0);
+        int lastRowNum = sheet.getLastRowNum();
+        Row row1 = sheet.getRow(lastRowNum + 3);
+        Row row2 = sheet.getRow(lastRowNum + 4);
+        ExcelStyleUtil excelStyleUtil = new ExcelStyleUtil(result);
+
+        int num = 1;
+        for (Map.Entry<String, List<Test>> entry : collect.entrySet()) {
+            if (null == row1) {
+                row1 = sheet.createRow(lastRowNum + 3);
+                Cell cell = row1.createCell(0);
+                cell.setCellStyle(excelStyleUtil.buildCellStyle(result, ExcelStyleUtil.REMARK_STYLES));
+            }
+            Cell cell1 = row1.createCell(num);
+            cell1.setCellValue(entry.getKey());
+            cell1.setCellStyle(excelStyleUtil.buildCellStyle(result, ExcelStyleUtil.DATA_STYLES));
+
+            if (null == row2) {
+                row2 = sheet.createRow(lastRowNum + 4);
+                Cell cell = row2.createCell(0);
+                cell.setCellValue("合计");
+                cell.setCellStyle(excelStyleUtil.buildCellStyle(result, ExcelStyleUtil.REMARK_STYLES));
+            }
+            Cell cell = row2.createCell(num);
+            cell.setCellValue(entry.getValue().size());
+            cell.setCellStyle(excelStyleUtil.buildCellStyle(result, ExcelStyleUtil.DATA_STYLES));
+            num++;
+        }
+
+
         //设置请求头,解决文件名中文乱码问题
         response.setCharacterEncoding("UTF-8");
         response.setHeader("content-Type", "application/vnd.ms-excel");
-        response.setHeader("Content-disposition","attachment;fileName="+ URLEncoder.encode(title,"utf-8"));
+        response.setHeader("Content-disposition", "attachment;fileName=" + URLEncoder.encode(title, "utf-8"));
         result.write(response.getOutputStream());
         // 清除response
         response.flushBuffer();
         result.close();
     }
-
 
 
     @GetMapping("/exportSheet")
