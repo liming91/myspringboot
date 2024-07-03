49675cf5932c4a231567547c31337d7609440bdc
liming <522724933@qq.com>
2024-07-03 22:26:03 +0800

    #lm 多态

--- src/test/java/com/ming/AnimalTest.java
+++ src/test/java/com/ming/AnimalTest.java
@@ -1,7 +1,10 @@
 package com.ming;
 
 import com.ming.entities.Animal;
+import com.ming.entities.Cat;
 import com.ming.entities.Dog;
+
+import java.sql.Connection;
 
 /**
  * 多态测试
@@ -12,12 +15,42 @@ import com.ming.entities.Dog;
 public class AnimalTest {
 
     public static void main(String[] args) {
+        //多态
         AnimalTest animalTest = new AnimalTest();
         animalTest.fun(new Dog());
+        animalTest.fun(new Cat());
+
+        animalTest.method(new Cat());
     }
 
+    //多态
     public void fun(Animal animal) {
         animal.eat();
         animal.shout();
     }
+
+    //如果没有多态性，必须创建子类对象
+    public void fun(Dog dog) {
+        dog.eat();
+        dog.shout();
+    }
+
+    public void fun(Cat cat) {
+        cat.eat();
+        cat.shout();
+    }
+
+    //有多态、随便new个对象可以通用 比如调用animalTest.method(new Cat());
+    public void method(Object obj){
+
+    }
+
+    //多态 jdbc中调用不同的数据库厂商,相当于在父类中定义了数据库连接，我们操作调用时只需要对应不同的数据库厂商
+     class Driver{
+        public void doData(Connection conn){//相当于 new MySqlConnection() 的子类
+                //按照规范操作数据
+                //conn.createStatement()
+        }
+    }
+
 }
