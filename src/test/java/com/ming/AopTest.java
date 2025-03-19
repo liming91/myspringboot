a6495261b7b027ddf7a9f4368e4b3ba05ffc9a05
liming <522724933@qq.com>
2025-03-19 15:19:57 +0800

    #lm 定时任务配置

--- src/test/java/com/ming/AopTest.java
+++ src/test/java/com/ming/AopTest.java
@@ -3,6 +3,7 @@ package com.ming;
 import com.ming.aop.JdkService;
 import com.ming.aop.Real;
 import com.ming.aop.ServiceInvocationHandler;
+import org.apache.log4j.Logger;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -31,7 +32,6 @@ import java.lang.reflect.Proxy;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class AopTest {
-
     /**
      *
      *
